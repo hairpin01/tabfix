86e26882a07c5c2445f2539c35bba7723255cc6b
Шмэлька <alichka240784@gmail.com>
2025-12-15 02:11:22 +1000

    gshshsyysua

--- src/tabfix/__init__.py
+++ src/tabfix/__init__.py
@@ -1,17 +1,7 @@
 __version__ = "1.0.0"
-__author__ = "hairpin01"
+__author__ = "Alina"
 __license__ = "GNU General Public License v3.0"
 
-try:
-    from .tabfix import main, TabFix, GitignoreMatcher, Colors
-except ImportError:
-    try:
-        from .__main__ import main, TabFix, GitignoreMatcher, Colors
-    except ImportError:
-        print("Warning: Could not import classes from tabfix module")
-        main = None
-        TabFix = None
-        GitignoreMatcher = None
-        Colors = None
+from .core import main, TabFix, GitignoreMatcher, Colors
 
 __all__ = ["main", "TabFix", "GitignoreMatcher", "Colors", "__version__"]
