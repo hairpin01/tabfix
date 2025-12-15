ca909d05676fefafcfcc0193f77137d53d2b4f96
Шмэлька <alichka240784@gmail.com>
2025-12-16 02:11:56 +1000

    Bump version from 1.2.2 to 1.2.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tabfix-tool",
-    version="1.2.2",
+    version="1.2.3",
     author="hairpin01",
     author_email="alichka240784@gmail.com",
     description="Advanced tool for fixing tab/space indentation issues",
