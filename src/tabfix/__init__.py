de298cffdabe1e10d3dc178d0f15eeb76ba53205
Шмэлька <alichka240784@gmail.com>
2025-12-18 20:11:52 +1000

    update __init__.py

--- src/tabfix/__init__.py
+++ src/tabfix/__init__.py
@@ -1,15 +1,20 @@
-__version__ = "1.2.5.2"
+__version__ = "1.5.4"
 
 from .core import TabFix, Colors, print_color, GitignoreMatcher
 from .config import TabFixConfig, ConfigLoader
-
+from .autoformat import Formatter, FormatterManager, FileProcessor, get_available_formatters, create_autoformat_config
 
 __all__ = [
     "TabFix",
-    "Colors", 
+    "Colors",
     "print_color",
     "GitignoreMatcher",
     "TabFixConfig",
     "ConfigLoader",
+    "Formatter",
+    "FormatterManager",
+    "FileProcessor",
+    "get_available_formatters",
+    "create_autoformat_config",
     "__version__",
 ]
